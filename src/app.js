5134050b196e6d57f7397562f78310dca3045cd1
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-09-14 23:53:57 -0500

    Register controller and their configs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import { configDotenv } from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
 configDotenv();
 const app = express();
 app.use(morgan("dev"));
-
+app.use("/api/v1", authRoutes);
+app.use(express.json());
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,19 @@
-export const register = () => {};
+import User from "../models/user.model.js";
 
-export const login = () => {};
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    const userSaved = await newUser.save();
+    res.send(userSaved);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+export const login = (req, res) => {};
