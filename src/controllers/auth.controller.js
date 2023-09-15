3d1df0488dbcb1d3614c3915aabb37bc63087522
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-09-15 01:23:45 -0500

    encrypted password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,16 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
+    const hashPass = await bcryptjs.hash(password, 5);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashPass,
     });
     const userSaved = await newUser.save();
     res.send(userSaved);
