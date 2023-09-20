2883f1b334c40cfb28852f55309a113ab0a7df86
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-09-20 00:29:19 -0500

    Tasks crud

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { authRequired } from "../middlewares/validateToken.js";
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  putTask,
+} from "../controllers/task.controller.js";
 
 const router = Router();
 
-router.get("/tasks");
+router.get("/tasks", authRequired, getTasks);
+router.post("/tasks", authRequired, createTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
+router.get("/tasks/:id", authRequired, getTask);
+router.put("/tasks/:id", authRequired, putTask);
 
 export default router;
