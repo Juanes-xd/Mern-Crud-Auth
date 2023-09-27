b8838f46ee7bd34c8cede8087c4a9c93b4c3a46e
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-09-27 16:06:27 -0500

    Cors and register front

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { configDotenv } from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import taskRoutes from "./routes/task.routes.js";
 configDotenv();
 const app = express();
+app.use(cors());
 app.use(morgan("dev"));
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", taskRoutes);
